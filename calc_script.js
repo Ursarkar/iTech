12496a9260960470616f904b2aa2fa8ec75da3bc
Ursarkar <sparta7777777777@gmail.com>
2019-07-20 00:01:53 +0300

    округление/%размеров таблицы

--- calc_script.js
+++ calc_script.js
@@ -1,28 +1,37 @@
 function numtarget(val){ 
-    document.getElementById("val").value+=val   
+    document.getElementById("val").value+=val;   
 } 
 
 function ans() 
 { 
-    var a = document.getElementById("val").value     
-    var b = eval(a)
+    var a = document.getElementById("val").value;  
+    var b = eval(a);
     if (b==Infinity){
-        document.getElementById("val").value = "Деление на 0"
-        setInterval("cl()", 3000)}
-    else{document.getElementById("val").value = b}
+        document.getElementById("val").value = "Деление на 0";
+        setInterval("cl()", 3000);}
+    else{document.getElementById("val").value = b.toFixed(5)}
+ /*   else{c=b.toString();
+        if (c[11]>=5){
+            c[10]=c[10]+1;
+            c=c.substring(0,11);
+        }
+        else{
+            c=c.substring(0,11);
+        }
+        document.getElementById("val").value=c;}*/
 } 
 
 function cl(){
-    document.getElementById("val").value=""
+    document.getElementById("val").value="";
 }
 
 function del(){
     var a=document.getElementById("val").value
    // var b = eval(a.slice(0,-1)) 
-   var b = a.slice(0,-1)
+   var b = a.slice(0,-1);
     if (b==undefined || b=='-'){
-        b=""}
-    document.getElementById("val").value = b
+        b="";}
+    document.getElementById("val").value = b;
 }
 
 var sleep=0;
@@ -32,10 +41,9 @@ document.onmousemove = activeUser;
 function activeUser() {
     sleep=0;
 }
+
 function updateChat() {
   if (sleep>=10) { 
         cl();
-        return;
   }
-
 }
